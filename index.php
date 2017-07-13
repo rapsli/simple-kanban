025eeac68c876c1ce17306a61f9c04955dbfc4ab
Joshua Walcher <joshuawalcher@yahoo.com>
2017-07-13 07:36:25 -0500

    Update index.php

--- index.php
+++ index.php
@@ -15,7 +15,7 @@
 	<!-- Modal -->
 <div id="helpModal" class="modal hide fade" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
   <div class="modal-header">
-    <button type="button" class="close" data-dismiss="modal" aria-hidden="true">Ã—</button>
+    <button type="button" class="close" data-dismiss="modal" aria-hidden="true">x</button>
     <h3 id="myModalLabel">Help Project Dashboard</h3>
   </div>
   <div class="modal-body">
